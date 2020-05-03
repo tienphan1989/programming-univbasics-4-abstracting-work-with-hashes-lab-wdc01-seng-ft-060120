6867072f7e04e2f5e7632e344ae80c29cbd492fd
Tien Phan <tienphan1989@gmail.com>
2020-05-03 11:31:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,8 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash["key"]# given a hash an a key as parameters, return an updated hash
+  hash["key"] += 1
+  # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
 end
